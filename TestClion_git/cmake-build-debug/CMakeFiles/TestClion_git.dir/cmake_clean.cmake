347e493f1df256b21d82adb87306279ec450f15d
zqgs <502171653@qq.com>
2021-05-28 17:09:27 +0800

    git测试

---
+++ TestClion_git/cmake-build-debug/CMakeFiles/TestClion_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestClion_git.dir/main.cpp.obj"
+  "TestClion_git.exe"
+  "TestClion_git.exe.manifest"
+  "TestClion_git.pdb"
+  "libTestClion_git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestClion_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
