a6e1b7e167931208bee78fe65333d503cdaf1f75
zengqingguo <502171653@qq.com>
2021-05-30 19:56:18 +0800

    数据结构概论

---
+++ AbstractDataType/cmake-build-debug/CMakeFiles/AbstractDataType.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AbstractDataType"
+  "AbstractDataType.pdb"
+  "CMakeFiles/AbstractDataType.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AbstractDataType.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AbstractDataType/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/*
+ 数据结构及其算法学习--抽象数据类型ADT
+ 数据结构分为：逻辑、运算、存储(抽象数据类型忽略数据的存储)
+ 1.逻辑结构:数据对象及其关系
+ 2.运算:数据操作
+*/
+
+//逻辑结构的体现
+template <class T>
+class _Stack{
+public:
+    bool pop(T &Item);//出栈
+    bool push(const T& Item);//入栈
+    //...等等操作
+    void clear();
+};
+//运算则是对数据进行操作-->也就是函数的实现
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
